6e5d42b67a836a8c01b54c66e1bab555ac2e61a2
pecu <pecu610@gmail.com>
2016-10-11 10:11:52 +0800

    2010-10-11 PTT爬蟲練習

--- FirstStepInR.R
+++ FirstStepInR.R
@@ -6,6 +6,7 @@ Sys.setlocale(category = "LC_ALL", locale = "cht")
 startNo = 4637
 endNo   = 4641 
 subPath <- "https://www.ptt.cc/bbs/movie/index"
+alldata = data.frame()
 for( pid in startNo:endNo )
 {
   urlPath <- paste(subPath, pid, ".html", sep='')
@@ -14,19 +15,24 @@ for( pid in startNo:endNo )
   title   <- xpathSApply(xmldoc, "//div[@class=\"title\"]", xmlValue)
   title   <- gsub("\n", "", title)
   title   <- gsub("\t", "", title)
-  skipid  <- grep("(本文已被刪除)", title)
-  if( length(skipid) != 0 )
-  {
-    title   <- title[-skipid]
-  }
   path    <- xpathSApply(xmldoc, "//div[@class='title']/a//@href")
-  alldata <- data.frame(title, path)
-  write.table(alldata, file = "movie.csv")
+  Erroresult<- tryCatch({
+    subdata <- data.frame(title, path)
+    alldata <- rbind(alldata, subdata)
+  }, warning = function(war) {
+    print(paste("MY_WARNING:  ", urlPath))
+  }, error = function(err) {
+    print(paste("MY_ERROR:  ", urlPath))
+  }, finally = {
+    print(paste("End Try&Catch", urlPath))
+  })
 }
+write.table(alldata, file = "movie.csv")
 suburlPath <- "https://www.ptt.cc"
 for( i in 1:length(alldata[,1]) )
 {
-  ipath   <- paste(suburlPath, path[i], sep='')
+  ipath   <- paste(suburlPath, alldata$path[i], sep='')
+  print(ipath)
   content <- getURL(ipath, encoding = "big5")
   xmldoc  <- htmlParse(content)
   article <- xpathSApply(xmldoc, "//div[@id=\"main-content\"]", xmlValue)
